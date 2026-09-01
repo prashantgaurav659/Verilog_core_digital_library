39f0f6de6e90c9dbc34b3fc773b0f716f49addb6
prashantgaurav659 <prashantgaurav659@gmail.com>
2026-09-01 20:08:32 +0530

    Update and_gate.sv

--- and_gate.sv
+++ and_gate.sv
@@ -1,7 +1,11 @@
-module and_gate  (
-    input A,
-    input B,
-    output Y
+// Module ka naam aur uske ports define karna
+module and_gate (
+    input A,     // Pehla digital input
+    input B,     // Doosra digital input
+    output Y    // Digital output
 );
+
+    // Continuous Assignment: Y ko hamesha A aur B ka AND karke update rakho
     assign Y = A & B;
+
 endmodule
