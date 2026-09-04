86f23d4fa7b93b8109355bb56522479c71f691e8
prashantgaurav659 <prashantgaurav659@gmail.com>
2026-09-04 13:47:37 +0530

    Update full_adder_gate.sv

--- full_adder_gate.sv
+++ full_adder_gate.sv
@@ -4,11 +4,11 @@ module full_adder_gate (
   input b,//input bit 2
   input cin, //input bit 3
   output sum, //sum output (XOR logic)
-  output cout //carry output (AND logic)
+  output cout //cout output (AND logic)
 );
   //Sum bit logic
   assign sum = a ^ b ^ cin ;
-  //carry bit logic
+  //cout bit logic
   assign cout = (a & b) | (b & cin) | (cin & a) ;
 
 endmodule
