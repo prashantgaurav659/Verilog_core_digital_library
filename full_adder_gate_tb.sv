5de89037d62e342c4fc80629a4b075aff95f7d74
prashantgaurav659 <prashantgaurav659@gmail.com>
2026-09-04 13:55:02 +0530

    Update full_adder_gate_tb.sv

--- full_adder_gate_tb.sv
+++ full_adder_gate_tb.sv
@@ -20,28 +20,28 @@ module full_adder_gate_tb;
     
     $display("Time\t a \t b \t cin \t sum \t cout");
     $display("-----------------------");
-    //case1 : 0+0+0 = sum:0, carry:0
+    //case1 : 0+0+0 = sum:0, cout:0
     a = 0 ; b = 0; cin = 0; #10;
     $display("0dns\t %b \t %b \t %b \t %b \t %b", $time, a, b, cin,  sum, cout);
-    //case2 : 0+0+1 = sum:1, carry:0
+    //case2 : 0+0+1 = sum:1, cout:0
     a = 0 ; b = 0; cin = 1;  #10;
     $display("0dns\t %b \t %b \t %b \t %b \t %b", $time, a, b, cin,  sum, cout);
-     //case3 : 0+1+0 = sum:1, carry:0
+     //case3 : 0+1+0 = sum:1, cout:0
     a = 0 ; b = 1; cin = 0; #10;
     $display("0dns\t %b \t %b \t %b \t %b \t %b", $time, a, b, cin,  sum, cout);
-     //case4 : 0+1+1 = sum:0, carry:1
+     //case4 : 0+1+1 = sum:0, cout:1
     a = 0 ; b = 1; cin = 1; #10;
     $display("0dns\t %b \t %b \t %b \t %b \t %b", $time, a, b, cin,  sum, cout);
-     //case5 : 1+0+0 = sum:1, carry:0
+     //case5 : 1+0+0 = sum:1, cout:0
     a = 1 ; b = 0; cin = 0; #10;
     $display("0dns\t %b \t %b \t %b \t %b \t %b", $time, a, b, cin,  sum, cout);
-     //case6 : 1+0+1 = sum:0, carry:1
+     //case6 : 1+0+1 = sum:0, cout:1
     a = 1 ; b = 0; cin = 1; #10;
     $display("0dns\t %b \t %b \t %b \t %b \t %b", $time, a, b, cin,  sum, cout);
-     //case7 : 1+1+0 = sum:0, carry:1
+     //case7 : 1+1+0 = sum:0, cout:1
     a = 1 ; b = 1; cin = 0; #10;
     $display("0dns\t %b \t %b \t %b \t %b \t %b", $time, a, b, cin,  sum, cout);
-     //case8 : 1+1+1 = sum:1, carry:1
+     //case8 : 1+1+1 = sum:1, cout:1
     a = 1 ; b = 1; cin = 1; #10;
     $display("0dns\t %b \t %b \t %b \t %b \t %b", $time, a, b, cin,  sum, cout);
 
